3019af0e84a7f13212b1983038378ae1c12ee41a
Andrey Dantas <andreydantasvf@gmail.com>
2023-11-01 13:02:19 -0300

    feat: add enemy board

--- game/entities/screens/ships_selected_screen.rb
+++ game/entities/screens/ships_selected_screen.rb
@@ -11,10 +11,17 @@ class ShipsSelectedScreen
     @blocks = []
     @width_canvas = App.class_variable_get(:@@canvas).width
     @block_size = App.class_variable_get(:@@canvas).block_size
-    @ship = Ship.new('./images/aircraft_carrier.png', 5, ((@width_canvas - @cols * (@block_size + 2)) / 2), 250, @rows, @cols, @board)
+    @ship = Ship.new('./images/aircraft_carrier.png', 5, 25, 250, @rows, @cols, @board)
     @number_ships = 10
     @selecteds_ship = []
+
+    text_player = Text.new('Player', size: 26, y: 200)
+    text_player.x = (25 + @cols * (@block_size + 2) - text_player.width) / 2
+
+    text_enemy = Text.new('Enemy', size: 26, y: 200)
+    text_enemy.x = @width_canvas - text_enemy.width + 25 - (@cols * (@block_size + 2)) / 2
     
+    render_enemy_board
     render
   end
 
@@ -25,7 +32,7 @@ class ShipsSelectedScreen
     sub_title = Text.new('NAVIOS DISPONIVEIS: %d' %[@number_ships], size: 26, y: 120)
     sub_title.x = (@width_canvas - sub_title.width) / 2
 
-    x_displacement = (@width_canvas - @cols * (@block_size + 2)) / 2
+    x_displacement = 25
     y_displacement = 250
 
     (0...@rows).each do |row|
@@ -35,7 +42,21 @@ class ShipsSelectedScreen
     end
 
     @blocks.each(&:draw)
+
     @selecteds_ship.each { |ship| Image.new(ship.ship.path, width: ship.ship.width, height: ship.ship.height, z: ship.ship.z + 10, x: ship.ship.x, y: ship.ship.y, rotate: ship.ship.rotate)}
+  end
+
+  def render_enemy_board
+    x_displacement = @width_canvas - (25 + @cols * (@block_size + 2))
+    y_displacement = 250
+
+    (0...@rows).each do |row|
+      (0...@cols).each do |col|
+        @blocks << Block.new(x_displacement + col * (@block_size + 2), y_displacement + row * (@block_size + 2), 0)
+      end
+    end
+
+    @blocks.each(&:draw)
   end
 
   def select_ship
@@ -47,11 +68,11 @@ class ShipsSelectedScreen
       Window.clear
 
       if @number_ships < 5
-        @ship = Ship.new('./images/submarine.png', 2, ((@width_canvas - @cols * (@block_size + 2)) / 2), 250, @rows, @cols, @board)
+        @ship = Ship.new('./images/submarine.png', 2, 25, 250, @rows, @cols, @board)
       elsif @number_ships < 8
-        @ship = Ship.new('./images/destroyers.png', 3, ((@width_canvas - @cols * (@block_size + 2)) / 2), 250, @rows, @cols, @board)
+        @ship = Ship.new('./images/destroyers.png', 3, 25, 250, @rows, @cols, @board)
       elsif @number_ships < 10
-        @ship = Ship.new('./images/tankers.png', 4, ((@width_canvas - @cols * (@block_size + 2)) / 2), 250, @rows, @cols, @board)
+        @ship = Ship.new('./images/tankers.png', 4, 25, 250, @rows, @cols, @board)
       end
 
       render
