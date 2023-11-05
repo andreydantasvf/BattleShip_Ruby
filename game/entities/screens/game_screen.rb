04dfdb75d28fa5b89e9e4e995d5c098fff5b9d1f
Andrey Dantas <andreydantasvf@gmail.com>
2023-11-05 12:50:33 -0300

    feat: add position all enemy ships

--- game/entities/screens/game_screen.rb
+++ game/entities/screens/game_screen.rb
@@ -6,8 +6,10 @@ class GameScreen
     @rows = rows
     @cols = cols
     @board_player = board_player
+    @board_enemy = Array.new(@rows) { Array.new(@cols, 0) }
     @selecteds_ship = selecteds_ship
-    @blocks = []
+    @blocks_player = []
+    @blocks_enemy = []
     @block_size = App.class_variable_get(:@@canvas).block_size
     @width_canvas = App.class_variable_get(:@@canvas).width
 
@@ -23,13 +25,68 @@ class GameScreen
 
     (0...@rows).each do |row|
       (0...@cols).each do |col|
-        @blocks << Block.new(x_displacement + col * (@block_size + 2), y_displacement + row * (@block_size + 2), 0)
+        @blocks_player << Block.new(x_displacement + col * (@block_size + 2), y_displacement + row * (@block_size + 2), 0)
       end
     end
 
     render_enemy_board
-    @blocks.each(&:draw)
+    @blocks_player.each(&:draw)
     @selecteds_ship.each { |ship| Image.new(ship.ship.path, width: ship.ship.width, height: ship.ship.height, z: ship.ship.z + 10, x: ship.ship.x, y: ship.ship.y, rotate: ship.ship.rotate)}
+  end
+
+  def place_ship(board, size)
+    loop do
+      row = rand(board.length)
+      col = rand(board[0].length)
+
+      orientation = [:horizontal, :vertical].sample
+
+      valid_placement = true
+
+      if orientation == :horizontal
+        valid_placement = (col + size <= board[0].length)
+      else
+        valid_placement = (row + size <= board.length)
+      end
+
+      if valid_placement
+        if orientation == :horizontal
+          valid_placement = board[row][col...(col + size)].all? { |cell| cell == 0 }
+        else
+          valid_placement = (row...(row + size)).all? { |r| board[r][col] == 0 }
+        end
+      end
+
+      if valid_placement
+        if orientation == :horizontal
+          (col...(col + size)).each { |c| board[row][c] = 1 }
+        else
+          (row...(row + size)).each { |r| board[r][col] = 1 }
+        end
+        break
+      end
+    end
+  end
+
+
+
+  def update
+    @blocks_enemy.each(&:remove)
+    @blocks_enemy.each(&:draw)
+  end
+
+  def handle_click(row, col)
+    if @board_enemy[row][col] == 0
+      @board_enemy[row][col] = 2
+      @blocks_enemy[row * @cols + col].state = 2
+      puts "Tiro na água."
+      update
+    elsif @board_enemy[row][col] == 1
+      @board_enemy[row][col] = 2
+      @blocks_enemy[row * @cols + col].state = 1
+      puts "Você atingiu o navio inimigo!"
+      update
+    end
   end
 
   def render_enemy_board
@@ -44,10 +101,21 @@ class GameScreen
 
     (0...@rows).each do |row|
       (0...@cols).each do |col|
-        @blocks << Block.new(x_displacement + col * (@block_size + 2), y_displacement + row * (@block_size + 2), 0)
+        @blocks_enemy << Block.new(x_displacement + col * (@block_size + 2), y_displacement + row * (@block_size + 2), @board_enemy[row][col])
       end
     end
 
-    @blocks.each(&:draw)
+    @blocks_enemy.each(&:draw)
+
+    place_ship(@board_enemy, 5)
+    place_ship(@board_enemy, 4)
+    place_ship(@board_enemy, 4)
+    place_ship(@board_enemy, 3)
+    place_ship(@board_enemy, 3)
+    place_ship(@board_enemy, 3)
+    place_ship(@board_enemy, 2)
+    place_ship(@board_enemy, 2)
+    place_ship(@board_enemy, 2)
+    place_ship(@board_enemy, 2)
   end
 end
